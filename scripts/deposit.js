ae77c251d50dc44197ff88c16c16744961a6cb2e
Amy_Robrigado <ARobrigado@nyc.yearup.org>
2022-10-25 15:28:42 -0400

    Moved files and added a git keep

--- scripts/deposit.js
+++ scripts/deposit.js
@@ -7,7 +7,7 @@ function init() {
     const resBtn = document.getElementById("resBtn");
 
     calBtn.onclick = calBtnOnClick;
-    resBtn.onclick = resBtnOnClick;
+    // resBtn.onclick = resBtnOnClick;
 }
 
 /* Calculate Button */
@@ -17,13 +17,14 @@ function calBtnOnClick() {
     const intRate = document.getElementById("intRate");
     const numYears = document.getElementById("numYears");
 
-    let answer1 = Number(deposit.value);
-    let answer2 = Number(intRate.value);
-    let answer3 = Number(numYears.value);
+    let depNum = Number(deposit.value);
+    let intNum = Number(intRate.value);
+    let yearsNum = Number(numYears.value);
 
-    let apyPercent = answer2 / 100;
-    let apyAmount = answer1 (Math.pow(1 + (apyPercent / 365),(365 * answer3)));
-    let intEarned = apyAmount - answer1; 
+    let apyPercent = intNum / 100;
+    //let apyAmount = answer1 (Math.pow(1 + (apyPercent / 365),(365 * answer3)));
+    let apyAmount = depNum * (1 + intNum/yearsNum)Math.pow(yearsNum);
+    let intEarned = apyAmount - depNum; 
 
     const futValue = document.getElementById("futValue");
     const totInt = document.getElementById("totInt");
